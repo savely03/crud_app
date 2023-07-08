2879ac225245b6418454d2ca0474cbe5e6b44eb5
Savely Domnikov <savely.domnikov@yandex.ru>
2023-07-08 15:32:15 +0300

    Added some changes

--- src/main/java/ru/hogwarts/school/exception/AvatarNotFoundException.java
+++ src/main/java/ru/hogwarts/school/exception/AvatarNotFoundException.java
@@ -9,6 +9,6 @@ public class AvatarNotFoundException extends TypicalException {
 
     @Override
     public String getMessage() {
-        return "Данный автар не найден";
+        return "Данный аватар не найден";
     }
 }
