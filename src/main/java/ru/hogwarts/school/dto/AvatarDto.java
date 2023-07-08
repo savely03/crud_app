46062930ab0c24ca9be6ebf86eeb5de78947e691
Savely Domnikov <savely.domnikov@yandex.ru>
2023-07-08 15:31:08 +0300

    Added some changes

--- src/main/java/ru/hogwarts/school/dto/AvatarDto.java
+++ src/main/java/ru/hogwarts/school/dto/AvatarDto.java
@@ -12,4 +12,5 @@ import lombok.NoArgsConstructor;
 public class AvatarDto {
     private Long id;
     private Long studentId;
+    private String urlImage;
 }
--- src/main/java/ru/hogwarts/school/mapper/AvatarMapper.java
+++ src/main/java/ru/hogwarts/school/mapper/AvatarMapper.java
@@ -4,10 +4,17 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import ru.hogwarts.school.dto.AvatarDto;
 import ru.hogwarts.school.entity.Avatar;
+import ru.hogwarts.school.entity.Student;
 
 @Mapper
 public interface AvatarMapper {
     @Mapping(target = "studentId", source = "entity.student.id")
+    @Mapping(target = "urlImage", expression = "java(setUrlImage(entity.getStudent()))")
     AvatarDto toDto(Avatar entity);
+
     Avatar toEntity(AvatarDto avatarDto);
+
+    default String setUrlImage(Student student) {
+        return "http://localhost:8080/avatar/" + student.getId();
+    }
 }
